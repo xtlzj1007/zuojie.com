0b47382c9d62ccbd02a7ab1e2062eede2471add4
zuojie <zuojie1007@qq.com>
2017-12-23 15:45:30 +0800

    day5 model

--- day5/list/list.go
+++ day5/list/list.go
@@ -25,11 +25,24 @@ func createList(){
 }
 
 func createInHeader(h *Teacher,name string,age int)(*Teacher){
+	//链表从头部插入数据
 	p := &Teacher{}
 	p.Age = age
 	p.Name = name 
 	p.Next = h 
 	return p 
+}
+
+func createInTail(tail *Teacher,name string,age int)(*Teacher){
+	p := &Teacher{}
+	p.Age = age
+	p.Name = name 
+	if tail == nil{
+		return p 
+	}else{
+    	tail.Next = p
+		return p
+	}
 }
 
 func printList(h *Teacher){
@@ -43,11 +56,34 @@ func TestCreateInHeader(){
 	var header * Teacher
 	header = createInHeader(header,"a",12)
 	header = createInHeader(header,"b",20)
+	header = createInHeader(header,"c",33)
 	printList(header)
+}
+
+func TestCreateInTail(){
+	// var header * Teacher
+	tail := &Teacher{}
+	tail = createInTail(tail,"a",10)
+	tail = createInTail(tail,"b",12)
+	tail = createInTail(tail,"c",13)
+	printList(tail)
+}
+
+//构造函数
+func NewTeacher(name string,age int) *Teacher{
+	// return &Teacher{
+	// 	Name:name,
+	// 	Age:age,
+	// }
+	p := new(Teacher)
+	p.Name = name
+	p.Age = age
+	return p 
 }
 
 func main(){
 	// createList()
-	TestCreateInHeader()
+	// TestCreateInHeader()
+	TestCreateInTail()
 }
 
---
+++ day5/model/model.go
@@ -0,0 +1,13 @@
+package model
+
+type School struct{
+	Name string 
+	Addr string
+}
+
+func NewSchool(name,addr string) *School{
+	return &School{
+		Name:name,
+		Addr:addr,
+	}
+}
